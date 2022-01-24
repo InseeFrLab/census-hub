9ab831b4de710d9ac7ed8579b9b53abc8904f243
AgatheLM <agathe.le-meur@insee.fr>
2022-01-24 11:44:47 +0000

    Ajout des variables d'environnement pour l'image nsiws

--- script.sh
+++ script.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
+
+echo '\#^  </GlobalNamingResources>#i\
+          <Resource name="org.estat.sri.MySqlMappingStore" auth="Container" \n \
+          type="javax.sql.DataSource" username="'"$DB_MS_USER"'" password="'"$DB_MS_PWD"'" \n \
+          driverClassName="com.mysql.jdbc.Driver" \n \
+          url="jdbc:mysql://'"$DB_ADDRESS"'/'"$DB_MS_NAME"'" \n \
+          maxActive="64" maxIdle="64"/>'
+
 sed -i '\#^  </GlobalNamingResources>#i\
           <Resource name="org.estat.sri.MySqlMappingStore" auth="Container" \n \
-          type="javax.sql.DataSource" username="mauser" password="${1}" \n \
+          type="javax.sql.DataSource" username="'"$DB_MS_USER"'" password="'"$DB_MS_PWD"'" \n \
           driverClassName="com.mysql.jdbc.Driver" \n \
-          url="jdbc:mysql://10.132.15.230/msdb" \n \
-          maxActive="64" maxIdle="64"/>' /usr/local/tomcat/conf/server.xml     
+          url="jdbc:mysql://'"$DB_ADDRESS"'/'"$DB_MS_NAME"'" \n \
+          maxActive="64" maxIdle="64"/>' /usr/local/tomcat/conf/server.xml         
 sed -i '\#^  </GlobalNamingResources>#i\
           <Resource name="org.estat.sri.MySqlAuthDb" auth="Container" \n \
-          type="javax.sql.DataSource" username="mauser" password="mauserpwd" \n \
+          type="javax.sql.DataSource" username="'"$DB_AUTH_USER"'" password="'"$DB_AUTH_PWD"'" \n \
           driverClassName="com.mysql.jdbc.Driver" \n \
-          url="jdbc:mysql://10.132.15.230/authdb" \n \
+          url="jdbc:mysql://'"$DB_ADDRESS"'/'"$DB_AUTH_NAME"'" \n \
           maxActive="64" maxIdle="64"/>' /usr/local/tomcat/conf/server.xml
 /usr/local/tomcat/bin/catalina.sh run
+
+
